b224605fdf38f2609cc5af24341456fadfb9bf03
Gavin Liu <du20783@gmail.com>
2020-08-19 02:06:52 +0800

    add group create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,21 @@
 class GroupsController < ApplicationController
   def index 
     @groups = Group.all
+    @group = Group.new
   end
 
   def create 
-    @group = Group.new
+    @group = Group.create(group_params)
+    if @group.save
+      redirect_to groups_path, notice: "Group create success"
+    else
+      redirect_to groups_path, notice: "Group create false"
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name).merge(manager_id: current_user.id)
   end
 end
