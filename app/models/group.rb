8d15b3f4f7d7252dc4af2b03c30d49d5c014dddc
Gavin Liu <du20783@gmail.com>
2020-08-19 00:12:20 +0800

    fix model validates

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
-  validates :name, presence :true
+  validates :name, presence: true
+  validates :manager_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence :true
-  validates :email, presence :true
+  validates :username, presence: true
+  validates :email, presence: true
 end
