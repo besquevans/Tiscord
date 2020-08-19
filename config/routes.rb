5dc16bb21bfab0a210e2fed2e29f777383a855ad
Gavin Liu <du20783@gmail.com>
2020-08-20 00:02:15 +0800

    update baords routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "groups#index"
-  resources :groups, only: [:index, :show, :create, :update]
-  resources :boards, only: [:create, :update, :destroy]
+  resources :groups, only: [:index, :show, :create, :update] do 
+    resources :boards, only: [:create, :update, :destroy]
+  end
+  
   devise_for :users
 end
