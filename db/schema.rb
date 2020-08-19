42935f10dc189889112cfcfd51c0c38249b7d2a0
Gavin Liu <du20783@gmail.com>
2020-08-19 10:11:08 +0800

    add model Board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_154808) do
+ActiveRecord::Schema.define(version: 2020_08_19_020910) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
