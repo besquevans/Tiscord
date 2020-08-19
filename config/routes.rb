f6780f934fc5f7466d990ff1b2dc130f58fe97ef
Gavin Liu <du20783@gmail.com>
2020-08-20 00:34:00 +0800

    add messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "groups#index"
   resources :groups, only: [:index, :show, :create, :update] do 
-    resources :boards, only: [:create, :update, :destroy]
+    resources :boards, only: [:create, :update, :destroy] do 
+      resources :messages, only: [:create]
+    end
   end
   
   devise_for :users
