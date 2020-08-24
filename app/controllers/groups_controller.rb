55dff9bc0a3bd90728942648641b2c2a850fc77a
Gavin Liu <du20783@gmail.com>
2020-08-24 15:36:44 +0800

    fix group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,7 +20,8 @@ class GroupsController < ApplicationController
 
   def create 
     @group = Group.create(group_params)
-    if @group.save
+    @board = @group.boards.create(name: "First board")
+    if @group.save && @board.save
       redirect_to groups_path, notice: "Group create success"
     else
       redirect_to groups_path, notice: "Group create false"
@@ -31,5 +32,9 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name).merge(manager_id: current_user.id)
+  end
+
+  def board_params 
+    params.require(:board).permit(:name)
   end
 end
