1db026c7cf5ead9268f4d1a46cf09fe909145050
Gavin Liu <du20783@gmail.com>
2020-09-04 21:43:03 +0800

    add avatar for Group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_161636) do
+ActiveRecord::Schema.define(version: 2020_09_04_134147) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_161636) do
     t.integer "manager_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
   end
 
   create_table "messages", force: :cascade do |t|
