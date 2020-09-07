4362f544da890d7a00d54f586e18567cc9ca5cf1
Gavin Liu <du20783@gmail.com>
2020-09-07 14:08:53 +0800

    set owner sender to right site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  
+  before_action :set_user
+
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def set_user
+    if user_signed_in?
+      session[:user_id] = current_user.id
+    end
   end
 end
--- app/frontend/channels/board_channel.js
+++ app/frontend/channels/board_channel.js
@@ -10,11 +10,22 @@ document.addEventListener("turbolinks:load", () => {
     },
     received(data) {
       console.log("received!!!!!!")
+      // console.log(data)
+      // console.log(data.html)
+      const current_user = document.querySelector("#main-navbar").dataset.user
+      console.log(current_user)
+      console.log(data.sender)
+      if (data.sender == current_user) {
+        const dataArr = data.html.split("sender")
+        data.html = dataArr[0] + " me" + dataArr[1]
+      }
+
       document.querySelector("#message-items").innerHTML += data.html
       const messageItems = document.querySelector("#message-items")
       const messageForm = document.querySelector(".message-form")
       messageItems.scrollTop = messageItems.scrollHeight
       messageForm.querySelector("#message_content").value = ""
+      messageForm.commit.removeAttribute("disabled")
       // if (data.cableReady) CableReady.perform(data.operations);
     },
   });
