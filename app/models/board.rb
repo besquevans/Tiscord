eefbcf5d6308f4ac5d3fab994db29bc780a996d0
Gavin Liu <du20783@gmail.com>
2020-09-14 00:39:19 +0800

    add board_cannot_less_one for board

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,4 +3,15 @@ class Board < ApplicationRecord
 
   belongs_to :group
   has_many :messages, dependent: :destroy
+
+  before_destroy :board_cannot_less_one
+
+  private
+
+  def board_cannot_less_one
+    if self.group.boards.count == 1
+      errors[:base] << "cannot delete if less 1 board"
+      throw(:abort)
+    end
+  end
 end
