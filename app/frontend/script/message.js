e7448fd45921b79737b56ce7640c06ab787d4cc7
Gavin Liu <du20783@gmail.com>
2020-09-14 23:51:17 +0800

    update css for message form submit

--- app/frontend/script/message.js
+++ app/frontend/script/message.js
@@ -1,6 +1,16 @@
 document.addEventListener("turbolinks:load", () => {
   const messageItems = document.querySelector("#message-items")
+  const messageForm = document.querySelector(".message-form")
   const jumpToBottom = () => messageItems.scrollTop = messageItems.scrollHeight
 
-  if (messageItems){ jumpToBottom() }
+  if (messageItems) { jumpToBottom() }
+  if (messageForm) { messageForm.addEventListener("keyup", () => addSubmit() )}
+
+  const addSubmit = () => {
+    if (messageForm.querySelector(".message-input").value != "") {
+      messageForm.querySelector(".message-btn").style.display = "block"
+    } else {
+      messageForm.querySelector(".message-btn").style.display = "none"
+    }
+  }
 })
