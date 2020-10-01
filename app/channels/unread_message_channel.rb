a44e7ee346ba979173f1b48828643c6852c24d62
Gavin Liu <du20783@gmail.com>
2020-10-02 00:04:59 +0800

    add unread_message channel

---
+++ app/channels/unread_message_channel.rb
@@ -0,0 +1,9 @@
+class UnreadMessageChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
---
+++ test/channels/unread_message_channel_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class UnreadMessageChannelTest < ActionCable::Channel::TestCase
+  # test "subscribes" do
+  #   subscribe
+  #   assert subscription.confirmed?
+  # end
+end
