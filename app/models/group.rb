1d5bec7ad121d27c10bf788473452b8f334df768
Gavin Liu <du20783@gmail.com>
2020-10-19 19:11:15 +0800

    add group manager relastionship

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :manager_id, presence: true
 
+  belongs_to :manager, foreign_key: :manager_id, class_name: "User"  #群組的建立者
   has_many :groupusers, foreign_key: :group_id, dependent: :destroy  #記錄使用者與群組的中間表
   has_many :users, through: :groupusers   #群組裡的使用者
   has_many :boards, dependent: :destroy
