314a0092de5c0f5fa52c6a84c7b584acd8e47f1c
Gavin Liu <du20783@gmail.com>
2020-10-23 01:35:06 +0800

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "groups#index"
-  resources :groups, only: [:index, :create, :update] do
+  root "boards#show"
+  resources :groups, only: [:create, :update] do
     resources :boards, only: [:index, :show, :create, :update, :destroy] do
       resources :messages, only: [:create]
     end
