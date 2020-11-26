408d7dc0b2d591b2a47853f48f703d9d005cff8c
Gavin Liu <du20783@gmail.com>
2020-11-26 23:13:01 +0800

    invited user by invited code

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,15 +1,10 @@
 class BoardsController < ApplicationController
+  before_action :find_groups_and_boards, only: [:index, :show]
+
+  def index
+  end
+
   def show
-    @groups = current_user.groups #使用者參加的群組
-
-    if params[:group_id]
-      @group = Group.find(params[:group_id])
-    else
-      @group = current_user.groups.first
-    end
-    # @newgroup = Group.new
-
-    @boards = @group.boards
     if params[:id]
       @board = Board.find(params[:id])
     else
@@ -40,5 +35,17 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name)
+  end
+
+  def find_groups_and_boards
+    @groups = current_user.groups #使用者參加的群組
+
+    if params[:group_id]
+      @group = Group.find(params[:group_id])
+    else
+      @group = current_user.groups.first
+    end
+
+    @boards = @group.boards
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader   #carrierwave
 
   has_many :own_groups, foreign_key: :manager_id, class_name: "Group", dependent: :destroy #使用者建立的群組
-  has_many :groupusers, foreign_key: :user_id, dependent: :destroy  #記錄使用者與群組的中間表
+  has_many :groupusers, foreign_key: :user_id  #記錄使用者與群組的中間表
   has_many :groups, through: :groupusers   #使用者參加的群組
   has_many :messages, dependent: :destroy
 
+  after_create :join_invited_group
   after_create :create_own_group
+  attr_accessor :invited_code
 
   def create_own_group #建立預設群組
     group = Group.create(name: "my first group", manager_id: self.id)
@@ -23,5 +25,12 @@ class User < ApplicationRecord
 
   def avatar_url
     avatar.url ? avatar.url : "/default_avatar.png"
+  end
+
+  def join_invited_group   #加入邀請的群組
+    invited_group = Group.find_by(slug: self.invited_code)
+    if invited_group
+      invited_group.users << self
+    end
   end
 end
